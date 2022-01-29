44342eebe66067a3f310f79c1605aa6c52fbe0fb
Michael Yik <myik2@uwo.ca>
2022-01-29 01:19:05 -0500

    added Parcelable implementation and auto-methods into Bill.

--- RSplit/src/main/java/RSplit/java/Bill.java
+++ RSplit/src/main/java/RSplit/java/Bill.java
@@ -1,11 +1,42 @@
 package RSplit.java;
 import java.util.ArrayList;
 import android.os.Parcelable;
-public class Bill {
+import android.os.Parcel;
+
+public class Bill implements Parcelable{
     //data variables for monetary balance and tax percentage
     private double balance = 0.0;
     private double taxP = 0.0;
     //creating arraylist <type> Person class object for user profiles (names)
     private ArrayList<Person> Users;
 
+    protected Bill(Parcel in) {
+        balance = in.readDouble();
+        taxP = in.readDouble();
+        Users = in.createTypedArrayList(Person.CREATOR);
+    }
+
+    public static final Creator<Bill> CREATOR = new Creator<Bill>() {
+        @Override
+        public Bill createFromParcel(Parcel in) {
+            return new Bill(in);
+        }
+
+        @Override
+        public Bill[] newArray(int size) {
+            return new Bill[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeDouble(balance);
+        parcel.writeDouble(taxP);
+        parcel.writeTypedList(Users);
+    }
 }
